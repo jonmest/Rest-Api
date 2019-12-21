a51ee79d6dcf17e04e16649c17dc23e59077b1f3
jonmest <jc222rz@student.lnu.se>
2019-12-22 00:56:48 +0100

    saving

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const connectDB = require("./config/db.js");
 const errorHandler = require("./middleware/error.js");
 const cookieParser = require('cookie-parser')
 const { protect, authorize } = require('./middleware/auth')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xssClean = require('xss-clean')
 
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -29,6 +34,27 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("combined"));
 }
+
+// Sanitize data
+app.use(mongoSanitize())
+
+// Helmet for security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xssClean())
+
+// Rate limit
+const limiter = rateLimit({
+  windowMs: 10* 60 * 1000, // 10 min
+  max: 100
+})
+
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
+
 // Mount routers
 app.use('/api/v1/posts', posts)
 app.use('/api/v1/admin', admin)
